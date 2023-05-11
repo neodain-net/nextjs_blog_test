fabbfd21b68e776fe6c37683c18b6713034e44f9
neodain <neodain.net@gmail.com>
2023-05-11 21:52:06 +0900

    config.env.js add

--- src/lib/openai/request.js
+++ src/lib/openai/request.js
@@ -1,14 +1,15 @@
+import ENV from "../../../config.env.js";
+
 export async function getAllRooms() {
-  const { success, data } = await (
-    await fetch(`${process.env.BASE_URL}/room`)
-  ).json();
+  console.log(`baseurl : ${ENV.BASE_URL}`);
+  const { success, data } = await (await fetch(`${ENV.BASE_URL}/room`)).json();
   if (!success) throw new Error("Error fetching rooms");
   return data;
 }
 
 export async function getMessages(roomid) {
   const { success, data } = await (
-    await fetch(`${process.env.BASE_URL}/chat/${roomid}`)
+    await fetch(`${ENV.BASE_URL}/chat/${roomid}`)
   ).json();
   if (!success) throw new Error("Error fetching messages");
   return data;
@@ -16,7 +17,7 @@ export async function getMessages(roomid) {
 
 export async function createRoom() {
   const { success, data } = await (
-    await fetch(`${process.env.BASE_URL}/room`, {
+    await fetch(`${ENV.BASE_URL}/room`, {
       method: "POST",
     })
   ).json();
@@ -26,7 +27,7 @@ export async function createRoom() {
 
 export async function deleteRoom(roomid) {
   const { success, data } = await (
-    await fetch(`${process.env.BASE_URL}/room/${roomid}`, {
+    await fetch(`${ENV.BASE_URL}/room/${roomid}`, {
       method: "DELETE",
     })
   ).json();
@@ -38,7 +39,7 @@ export async function sendMessage({ roomid, message }) {
   if (!roomid && !message) throw new Error("Invalid arguments");
 
   const { success, data } = await (
-    await fetch(`${process.env.BASE_URL}/chat/${roomid}`, {
+    await fetch(`${ENV.BASE_URL}/chat/${roomid}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
