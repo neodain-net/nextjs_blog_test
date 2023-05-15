052b86097dec49d58181b06439a6abcaae3f1fe3
neodain <neodain.net@gmail.com>
2023-05-15 22:22:06 +0900

    config.env.js url fix

--- config.env.js
+++ config.env.js
@@ -1,4 +1,4 @@
 export default {
-  BASE_URL: "https://neodain.up.railway.app:3000/api",
-  // BASE_URL: "http://localhost:3000/api",
+  // BASE_URL: "https://neodain.up.railway.app:3000/api",
+  BASE_URL: "http://localhost:3000/api",
 };
