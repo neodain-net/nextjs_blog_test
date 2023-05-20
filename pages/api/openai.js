efdc5aa5add40c4c4260f724dd562bf11f28977a
neodain <neodain.net@gmail.com>
2023-05-20 23:24:41 +0900

    openai import path fix

--- pages/api/openai.js
+++ pages/api/openai.js
@@ -1,9 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import connect from "../../../database/conn";
+import connect from "../../database/conn";
 import {
   getAllRooms,
   createRoom,
-} from "../../../src/lib/openai/controller/room.controller";
+} from "../../src/lib/openai/controller/room.controller";
 
 export default async function handler(req, res) {
   connect().catch((err) =>
