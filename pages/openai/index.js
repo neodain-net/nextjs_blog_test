79af8d6d13d1ca80b85240d9f32ed54e81e5dfdd
김홍태님/과금 Data <P161473@sktelecom.com>
2023-05-23 17:10:30 +0900

    ui fix

--- pages/openai/index.js
+++ pages/openai/index.js
@@ -39,16 +39,19 @@ export default function Openai() {
       <Head>
         <title>Neodain's OpenAI</title>
       </Head>
-      {/* <div className="grid"> */}
+      {/* // grid-cols-6 : grid를 사용하여 col수를 6개로 한다. */}
+      {/* <div className="grid grid-cols-6"> */}
       <div className="flex">
         <div
+          // col-span-6 : 6개의 cols들에서 6개의 cols를 사용한다
           className={
-            openaiBtn ? "grid-cols-6 col-span-6" : "grid-cols-6 col-span-6"
+            // openaiBtn ? "col-span-6" : "col-span-6"
+            openaiBtn ? "w-full sm:w-60" : "w-0 sm:w-60"
           }
         >
           {/* <div className="hidden md:grid bg-gray-900 md:col-span-1 z-10 text-gray-50"> */}
           {/* <div className="md:grid bg-gray-900 sm:w-60 md:col-span-1 z-10 text-gray-50"> */}
-          <div className="md:grid bg-gray-900 sm:w-60 z-10 text-gray-50">
+          <div className="bg-gray-900 sm:w-60 z-10 text-gray-50">
             {data && (
               <Aside
                 getRooms={data}
@@ -58,8 +61,10 @@ export default function Openai() {
             )}
           </div>
         </div>
-        <div className={openaiBtn ? "col-span-6" : "col-span-6"}>
-          <div className="bg-gray-800 text-gray-50 col-span-6 sm:pl-2 md:col-span-5 min-h-screen h-full mb-40">
+        {/* <div className={openaiBtn ? "col-span-6" : "col-span-6"}> */}
+        <div className={openaiBtn ? "w-full sm:flex" : "w-full sm:flex"}>
+          {/* <div className="bg-gray-800 text-gray-50 col-span-6 sm:pl-2 md:col-span-5 min-h-screen h-full mb-40"> */}
+          <div className="bg-gray-800 text-gray-50 sm:pl-2 min-h-screen h-full min-w-full mb-40">
             {roomid ? <Chat roomid={roomid}></Chat> : <Banner />}
             {roomid && <Search roomid={roomid}></Search>}
           </div>
--- src/components/openai/main.js
+++ src/components/openai/main.js
@@ -20,20 +20,23 @@ export default function Main({ roomid }) {
   if (messages.length === 0) return <NotFound></NotFound>;
 
   return (
-    <main className="container px-10 sm:px-20 lg:px-10 z-10">
-      {/* <div className="mx-auto w-3/5 py-5"> */}
-      <div className="mx-auto w-full py-5">
-        {messages &&
-          messages.map((message, index) => {
-            return (
-              <div key={index}>
-                {/* Ask Component */}
-                <Ask q={message.question}></Ask>
-                {/* Response */}
-                <Response ans={message.answer}></Response>
-              </div>
-            );
-          })}
+    // <main className="container px-5 sm:px-20 lg:px-10 z-10">
+    <main className={styles.container}>
+      <div className="px-5 md:px-20 lg:px-10 z-10">
+        {/* <div className="mx-auto w-3/5 py-5"> */}
+        <div className="mx-auto w-full py-5">
+          {messages &&
+            messages.map((message, index) => {
+              return (
+                <div key={index}>
+                  {/* Ask Component */}
+                  <Ask q={message.question}></Ask>
+                  {/* Response */}
+                  <Response ans={message.answer}></Response>
+                </div>
+              );
+            })}
+        </div>
       </div>
     </main>
   );
