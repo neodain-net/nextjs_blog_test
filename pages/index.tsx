0df81825da89f935b3d45f00497df60a469be848
김홍태님/과금 Data <P161473@sktelecom.com>
2023-06-21 18:08:18 +0900

    delete react-player

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import ReactPlayer from "react-player";
+// import ReactPlayer from "react-player";
 
 export default function LandingPage() {
   const router = useRouter();
